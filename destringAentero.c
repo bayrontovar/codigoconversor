9617e77ce176f4a78f94270432e3dfa4adbc0bf0
Bayron Tovar <bayron.tovar@gmail.com>
2019-08-30 17:47:46 -0500

    Este es el primer codigo de la cabecera

--- destringAentero.c
+++ destringAentero.c
@@ -38,6 +38,8 @@ for(i=0;i<tam2;i++){// de string a entero dato2
 	    if(dato2[i]==natu[j]) num2[tam2-1-i]=j;       
     }
 }
+
+pto=suma1-suma2;
 suma1=pot(num1, tam1);
 suma2=pot(num2, tam2);
 sumat=suma1+suma2+suma1+suma2;
